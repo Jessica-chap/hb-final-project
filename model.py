55b15619810c9056f4505013dcf6f7d414f58b06
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-04-06 02:58:26 +0000

    Created outline and wkg on model.py

--- model.py
+++ model.py
@@ -4,6 +4,66 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+##TO DO 
+###Work on relationships in tables
+
+
+class User(db.Model):
+    """A user"""
+##add null values##
+##for weight, kg or lb##
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, autoincrement=True,
+                        primary_key=True)
+    user_name = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    user_age = db.Column(db.Integer)
+    user_weight = db.Column(db.Integer)
+    user_gender = db.Column(db.String)
+
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} user_name={self.user_name}>'
+
+
+class Workout(db.Model):
+    """User Workout"""
+
+    __tablename__ = 'workouts'
+
+    workout_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    workout_date = db.Column(db.datetime)
+
+
+    def __repr__(self):
+        return f'<Workout workout_id={self.workout_id} workout_date={self.workout_date}>'
+
+
+class Workout_exercise(db.Model):
+    """Exercise specific for workout"""
+
+    __tablename__ = 'workout_exercises'
+
+    ##TODO complete table columns/repr
+
+
+class Exercises(db.Model):
+    """Specific exercise details"""
+
+    __tablename__ = 'exercises'
+
+    ##TODO complete tabel columns/repr
+
+    
+
+
+
+
+
+
 
 
 
