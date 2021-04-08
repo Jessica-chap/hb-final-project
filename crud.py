595969cc3cc028aa615e2ad4da8112ae46c5b097
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-04-08 03:09:12 +0000

    Error that cannot dropdb workouts being accessed by another user still need to test last two funcitons on crud.py first two are good after testing

--- crud.py
+++ crud.py
@@ -1,7 +1,10 @@
 """CRUD operations"""
 
 from model import db, User, Workout, Workout_exercise, Exercise, connect_to_db
+from datetime import datetime
 
+
+#TODO fix dbworkouts and test last two functions
 
 
 def create_user(user_name, password, user_age, user_weight, user_zipcode ):
@@ -16,6 +19,9 @@ def create_user(user_name, password, user_age, user_weight, user_zipcode ):
 
     return user
 
+# riley = create_user(user_name='riley', password='dogs', user_age='101', 
+#                 user_weight='100', user_zipcode='48076')
+
 
 def create_exercise(exercise_name, exercise_info):
     """create and return exercise"""
@@ -28,24 +34,28 @@ def create_exercise(exercise_name, exercise_info):
 
     return exercise
 
+# boat_row = create_exercise(exercise_name='boat_row', exercise_info='row row row the boat')
+
 
 
 def create_workout(user, workout_date):
     """create and return workout"""
 
-    workout = Workout(user_id=user.user_id, workout_date=workout_date)
+    workout = Workout(user=user.user_id, workout_date=workout_date)
 
     db.session.add(workout)
     db.session.commit()
 
     return workout
 
+# riley_wrkt = create_workout(user= riley.user_id, workout_date= datetime.now())
+
 
 def create_workout_exercise(workout, exercise, we_sets, we_reps, we_weight, we_equipment):
     """create and return workout_exercise"""
 
-    workout_exercise = Workout_exercise(workout_id=workout.workout_id,
-                        exercise_id=exercise.exercise_id,
+    workout_exercise = Workout_exercise(workout=workout.workout_id,
+                        exercise=exercise.exercise_id,
                         we_sets=we_sets, we_reps=we_reps,
                         we_weight=we_weight, 
                         we_equipment=we_equipment)
@@ -55,9 +65,11 @@ def create_workout_exercise(workout, exercise, we_sets, we_reps, we_weight, we_e
 
     return workout_exercise
 
-
-
-
+# wrkt_boat_row = create_workout_exercise(workout= riley_wrkt.workout_id, 
+#                                 exercise= boat_row.exercise_id,
+#                                 we_sets= 1, we_reps=60, we_repunit='minutes',
+#                                 we_weight=1, we_weightunit= 'bodyweight',
+#                                 we_equipment='none')
 
 
 
