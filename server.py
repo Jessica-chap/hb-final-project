67f466ba00377a34b40cb4ad7d53a4723745c012
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-04-11 01:38:51 +0000

    Have skeleton of route from homepage to create user back to log in then profile page

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = "sun"
 app.jinja_env.undefined = StrictUndefined
 
-
+##TODO work on more details for earch route with profile- login and create
 
 @app.route('/')
 def homepage():
@@ -18,6 +18,50 @@ def homepage():
     return render_template('homepage.html')
 
 
+@app.route('/create_user')
+def create_new_user():
+    """Create new user."""
+
+
+    return render_template('create_user.html')
+
+
+@app.route('/new_users', methods=['POST'])
+def new_user():
+
+    #TODO handle data being returned
+
+    return redirect('/', flash('Success! Login!'))
+
+
+
+@app.route('/users', methods=['POST'])
+def login_user():
+
+    user_name = request.form.get('user_name')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_user_name(user_name)
+
+    return redirect('/user_profile')
+
+
+@app.route('/user_profile')
+def user_profile_page():
+    """Users profile page."""
+
+    return render_template('user_profile.html')
+
+
+
+##TODO NEED CRUD DEF
+# @app.route('/users/<user_id>')
+# def show_user(user_id):
+#     """Show profile page of specific user."""
+
+#     user = crud.get_user_by_id(user_id)
+
+#     return render_template('user_details.html', user=user)
 
 
 
