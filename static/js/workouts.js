7b4390a210d467eb697d98d760c3a9dd1d982496
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-04-28 22:10:56 +0000

    new files added to git, img, js, saved_workout and test_data

---
+++ static/js/workouts.js
@@ -0,0 +1,59 @@
+"use strict";  
+
+
+  const addItemToTable = (exercise, sets, reps, 
+                          repunit, weight,
+                          weightunit, equipment, info) => {
+      const infoPopover =
+       `<a tabindex="0" class="bi bi-info-circle" role="button" data-toggle="popover" data-trigger="focus" title="${info}"  data-placement="left" data-content="test"></a>`
+      //console.log(infoPopover)
+      console.log("inside add item to table");
+    $('#user-exercise-selections').append("<tr>" +
+            "<td><a href='#'class='popover-anchor' data-toggle='popover'>" + exercise + "</a></td>" +
+            "<td>" + sets + "</td>" +
+            "<td>" + reps + "</td>" +
+            "<td>" + repunit + "</td>" +
+            "<td>" + weight + "</td>" +
+            "<td>" + weightunit + "</td>" +
+            "<td>" + equipment + "</td>" +
+            "</tr>");
+  
+
+            $('.popover-anchor').popover({
+              title: `${exercise}`,
+              content: `<p>${info}</p>`,
+              html: true
+            });        
+          };
+
+  $('#exercise-form').on('submit', (evt) => {
+    evt.preventDefault();
+    const formInputs = {
+      //'server .get requests : assign from HTML
+        'api_exercise_selection': $("#api_exercise_selection").val(),
+        'exercise_sets': $("#exercise_sets").val(),
+        'exercise_reps': $("#exercise_reps").val(),
+        'exercise_repunit': $("#exercise_repunit").val(),
+        'exercise_weight': $("#exercise_weight").val(),
+        'exercise_weightunit': $("#exercise_weightunit").val(),
+        'api_exercise_equipment': $("#api_exercise_equipment").val() 
+    }
+       
+    $.post("/add_exercise", formInputs, (res) => {
+      
+      addItemToTable(res.api_exercise_selection, 
+                    res.exercise_sets, res.exercise_reps, 
+                    res.exercise_repunit, res.exercise_weight, 
+                    res.exercise_weightunit, 
+                    res.api_exercise_equipment, res.exercise_info);
+      
+    });
+
+  });
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  });
+
+ 
+
