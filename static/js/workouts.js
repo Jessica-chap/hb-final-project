cdb4e7aa0fd797df2ef86ad6a7918c9cdf234753
Jessica Chapdelaine <jessicachap223@gmail.com>
2021-05-06 18:07:33 +0000

    adding pics for readme

--- static/js/workouts.js
+++ static/js/workouts.js
@@ -70,3 +70,13 @@
   function addExercise() {
     
   }
+
+  const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#input-password');
+
+  togglePassword.addEventListener('click', function (e){
+    
+    const type = password.getAttribute('type') === 'password' ? 'text':'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bi-eye-slash');
+  });
